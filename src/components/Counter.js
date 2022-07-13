fe8829a439ac3953fc9331773e7e1a010f1b9dbc
dhaval-at-tst <dhaval.tst@gmail.com>
2022-07-13 18:25:09 +0530

    D_13_07_2022

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,12 +1,46 @@
+import { useSelector, useDispatch } from "react-redux";
 import classes from './Counter.module.css';
+import { counterActions } from './../store/index';
 
 const Counter = () => {
-  const toggleCounterHandler = () => {};
+  const dispatch = useDispatch();
+  const counter = useSelector(state => state.counter.counter);
+  const showCounter = useSelector(state => state.counter.showCounter);
+
+  const toggleCounterHandler = () => {
+    dispatch(counterActions.toggleCounter())
+  };
+
+  const incrementHandler = () => {
+    dispatch(counterActions.increment())
+  };
+  const decrementHandler = () => {
+    dispatch(counterActions.decrement())
+  };
+  const increaseHandler = () => {
+    dispatch(counterActions.increse(5))
+  };
+  const decreaseHandler = () => {
+    dispatch(counterActions.decrese(5))
+  };
+  const resetHandler = () => {
+    dispatch(counterActions.reset)
+  };
+
 
   return (
     <main className={classes.counter}>
       <h1>Redux Counter</h1>
-      <div className={classes.value}>-- COUNTER VALUE --</div>
+      {showCounter && <div className={classes.value}>{counter}</div>}
+      <div>
+        <button onClick={incrementHandler}>Increment</button>
+        <button onClick={decrementHandler}>Decrement</button>
+        <button onClick={resetHandler}>Reset</button>
+      </div>
+      <div>
+        <button onClick={increaseHandler}>Increase by 5</button>
+        <button onClick={decreaseHandler}>Decrease by 5</button>
+      </div>
       <button onClick={toggleCounterHandler}>Toggle Counter</button>
     </main>
   );
--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
+import { Provider } from "react-redux";
 import React from 'react';
 import ReactDOM from 'react-dom';
 
 import './index.css';
 import App from './App';
+import store from './store/index';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<Provider store={store} ><App /></Provider>, document.getElementById('root'));
